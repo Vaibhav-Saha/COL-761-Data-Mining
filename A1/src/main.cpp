0132377ca9364e63fed74aafa1aa69c9a5978381
me <cs1200403@iitd.ac.in>
2023-08-21 20:48:24 +0530

    multiple compression

--- A1/src/main.cpp
+++ A1/src/main.cpp
@@ -3,17 +3,37 @@ using namespace std;
 
 #include "fptree.hpp"
 
-void compress(vector<pair<int,set<Item>>> &freq, Item input_file, Item output_file){
+bool static cmp(pair<int,set<Item>> a, pair<int,set<Item>> b){
+    return a.first<b.first;
+}
+
+vector<pair<int,set<Item>>> converter(set<pair<set<Item>,uint64_t>> freq){
+
+    vector<pair<int,set<Item>>> ans={};
+    for(auto i : freq){
+        if(i.first.size()==1){
+            continue;
+        }
+        ans.push_back(make_pair(i.first.size(),i.first));
+    }
+    sort(ans.begin(),ans.end(),cmp);
+    return ans;
+
+}
+
+vector<Transaction> compress(vector<pair<int,set<Item>>> &freq, Item input_file, Item output_file, int &count, map<string,set<string>> &mapping){
     ifstream data(input_file);
-    ofstream result(output_file);
     Item str;
 
     int size_freq = freq.size();
-    map<string,set<string>> mapping;
+    
+    map<set<string>,string> rev_map;
     vector<string> data_out;
-    int buffer = 0;
+    vector<Transaction> Transactions;
+    
     while(getline(data,str)){
         stringstream ss(str);
+        vector<Item> vec;
         Item num;
 
         unordered_map<string,int> maps={};
@@ -21,7 +41,6 @@ void compress(vector<pair<int,set<Item>>> &freq, Item input_file, Item output_fi
             maps[num]=1;
         }
 
-        int cnt=-1;
         Item ans="";
         for(int i=size_freq-1;i>=0;i--){
             set<string> s = freq[i].second;
@@ -36,26 +55,42 @@ void compress(vector<pair<int,set<Item>>> &freq, Item input_file, Item output_fi
                 }
             }
             if(pos){
-                mapping[to_string((i-size_freq))] = s;
+                string k="";
+                if(rev_map.find(s)==rev_map.end()){  
+                    mapping[to_string(count)] = s;
+                    rev_map[s]=to_string(count);
+                    k=to_string(count);
+                    count--;
+                }
+                else{
+                    k = rev_map[s];
+                }
                 for(auto it=s.begin();it!=s.end();it++){
                     maps.erase((*it));
                 }
-                ans+= to_string((i-size_freq) ) +" ";
+                ans+= k +" ";
+                vec.push_back((Item)k);
             }
 
         }
 
         for(auto it=maps.begin();it!=maps.end();it++){
             ans+= (*it).first +" ";
+            vec.push_back((Item)(*it).first);
         }
 
-        // result<<ans<<endl;
         data_out.push_back(ans);
     }
     
-    result<< to_string(mapping.size())<<endl;
+    //writing to new file
+    ofstream result;
+    ofstream damdam;
+    damdam.open("damru.dat", std::ofstream::out | std::ofstream::trunc);
+    result.open(output_file, std::ofstream::out | std::ofstream::trunc);
+
+    if(mapping.size()!=0){result<<to_string(mapping.size())<<endl;}
     for(auto it=mapping.begin();it!=mapping.end();it++){
-        Item ans= (*it).first +" : ";
+        Item ans= (*it).first +" ";
         set<string> &s = (*it).second;
         for(auto a: s){
             ans+= a +" ";
@@ -65,9 +100,49 @@ void compress(vector<pair<int,set<Item>>> &freq, Item input_file, Item output_fi
     }
     for(int i=0;i<data_out.size();i++){
         result<< data_out[i]<<endl;
+        damdam<<data_out[i]<<endl;
     }
-    // return mapping;
+
+    return Transactions;
+}
+
+void multi_compress(Item input_file, Item output_file){
+
+    Item item;
+    vector<Transaction> transactions;
+    ifstream data(input_file);
+    ofstream result(output_file);
+
+    Item str;
+    long long num_lines=0;
+    while(getline(data,item)){
+        vector<Item> vec;
+        stringstream itemset(item);
+        Item s;
+        num_lines++;
+        while(itemset>>s){
+            vec.push_back(s);
+        }
+        transactions.push_back(vec);
+    }
+
+    int percentage = 90;
+    int cnt=-1;
+    Item damru = "damru.dat";
+    map<string,set<string>> mapping;
+    while(percentage>1){
+        cout<<"fghj"<<endl;
+        unsigned long long minimum_support_threshold = num_lines*percentage/100;
+        FPTree fptree{ transactions, minimum_support_threshold };
+        std::set<Pattern> Freq1 = fptree_growth( fptree );
+        vector<pair<int,set<Item>>> freq = converter(Freq1);
+
+        transactions=compress(freq, input_file, output_file, cnt,mapping);
+        input_file = damru;
+        percentage-=5;
     
+    }
+
 }
 
 void decompress( Item input_file, Item output_file){
@@ -87,59 +162,36 @@ void decompress( Item input_file, Item output_file){
         ss>>id;
         set<string> s;
         mapping[id] = s;
-        ss>>num;
         while(ss>>num){
             mapping[id].insert(num);
         }
     }
 
-    // for(auto it= mapping.begin();it!=mapping.end();it++){
-    //     cout<< (*it).first<<" : ";
-    //     set<string> s = (*it).second;
-    //     for(auto x:s){
-    //         cout<< x<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-
     while(getline(data,str)){
         stringstream ss(str);
         Item num;
         Item ans ="";
+        set<int> sets;
         while(ss>>num){
-            if(stoi(num)>=0){
-                ans+= num +" ";
-            }else{
-                set<string> &s = mapping[num];
-                for(auto it: s){
-                    ans+= (it)+" ";
-                }
-            }
+            sets.insert(stoi(num));
+        }
 
+        while(!sets.empty() && *sets.begin()<0){
+            int x = *sets.begin();
+            sets.erase(sets.begin());
+            set<string> &m = mapping[to_string(x)];
+            for(auto it:m){
+                sets.insert(stoi(it));
+            }
+        }
+
+        for(auto it:sets){
+            ans += to_string(it)+ " ";
         }
         result<< ans<<endl;
     }
     
 }
-
-bool static cmp(pair<int,set<string>> a, pair<int,set<string>> b){
-    return a.first<b.first;
-}
-
-vector<pair<int,set<Item>>> converter(set<pair<set<Item>,uint64_t>> freq){
-
-    vector<pair<int,set<Item>>> ans={};
-    for(auto i : freq){
-        if(i.first.size()==1){
-            continue;
-        }
-        ans.push_back(make_pair(i.first.size(),i.first));
-    }
-    sort(ans.begin(),ans.end(),cmp);
-    return ans;
-
-}
-
 
 int main(int argc, char* argv[]) {
     if (argc != 4) {
@@ -151,33 +203,10 @@ int main(int argc, char* argv[]) {
     Item input_file = argv[2];
     Item output_file = argv[3];
 
-    Item item;
-    vector<Transaction> transactions;
-    ifstream data(input_file);
-    // ofstream result(output_file);
-    Item str;
-    long long count=0;
-    while(getline(data,item)){
-        vector<Item> vec;
-        stringstream itemset(item);
-        Item s;
-        count++;
-        while(itemset>>s){
-            vec.push_back(s);
-        }
-        transactions.push_back(vec);
-    }
 
-    const uint64_t minimum_support_threshold = count*90/100;
-
-    const FPTree fptree{ transactions, minimum_support_threshold };
-
-    const std::set<Pattern> Freq1 = fptree_growth( fptree );
-
-    vector<pair<int,set<Item>>> freq = converter(Freq1);
-        
     if(command=="C"){
-        compress(freq, input_file, output_file);
+        multi_compress(input_file,output_file);
+        // compress(freq, input_file, output_file, cnt);
     }else{
         decompress( input_file, output_file);
     }
